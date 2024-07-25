3f642ea953f1f1f541e6a9d5cf58cc54397a088d
Marcelo Nogueira <marcelo.bojikian@gmail.com>
2024-07-25 22:06:06 +0100

    Changing the name of the database backup file

--- scripts/mysql/backup.sh
+++ scripts/mysql/backup.sh
@@ -14,6 +14,6 @@ BACKUP_FILENAME="$BACKUP_DIR/$DB_DATABASE-$DATE.sql"
 
 sudo docker run --rm --network $NETWORK $MYSQL_IMAGE /usr/bin/mysqldump --no-tablespaces -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_DATABASE > $BACKUP_FILENAME
 
-cp $BACKUP_FILENAME "$BACKUP_DIR/backup.sql"
+cp $BACKUP_FILENAME "$BACKUP_DIR/database.sql"
 
 gzip $BACKUP_FILENAME
--- scripts/mysql/restore.sh
+++ scripts/mysql/restore.sh
@@ -5,6 +5,6 @@ source files/env/default
 BACKUP_DIR="files/backups"
 MYSQL_IMAGE="mysql:8.0"
 
-BACKUP_FILENAME="$BACKUP_DIR/backup.sql"
+BACKUP_FILENAME="$BACKUP_DIR/database.sql"
 
 sudo docker exec -i $DB_HOST mysql -h $DB_HOST -u $DB_USER -p$DB_PASS --force  $DB_DATABASE < $BACKUP_FILENAME
