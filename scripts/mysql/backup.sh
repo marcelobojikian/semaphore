3c5637394d52fbdea52367c62c157fe507484ef5
Marcelo Nogueira <marcelo.bojikian@gmail.com>
2024-07-29 14:52:37 +0100

    Update mysql backup and restore

--- scripts/mysql/backup.sh
+++ scripts/mysql/backup.sh
@@ -4,7 +4,7 @@ source files/env/default
 
 DATE=$(date +%F-%H%M%S)
 
-BACKUP_DIR="files/backups"
+BACKUP_DIR="files/database"
 MYSQL_IMAGE="mysql:8.0"
 NETWORK="docker_backend"
 
@@ -14,6 +14,6 @@ BACKUP_FILENAME="$BACKUP_DIR/$DB_DATABASE-$DATE.sql"
 
 sudo docker run --rm --network $NETWORK $MYSQL_IMAGE /usr/bin/mysqldump --no-tablespaces -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_DATABASE > $BACKUP_FILENAME
 
-cp $BACKUP_FILENAME "$BACKUP_DIR/database.sql"
+cp $BACKUP_FILENAME "$BACKUP_DIR/$DB_DATABASE.sql"
 
 gzip $BACKUP_FILENAME
--- scripts/mysql/restore.sh
+++ scripts/mysql/restore.sh
@@ -2,9 +2,9 @@
 
 source files/env/default
 
-BACKUP_DIR="files/backups"
+BACKUP_DIR="files/database"
 MYSQL_IMAGE="mysql:8.0"
 
-BACKUP_FILENAME="$BACKUP_DIR/database.sql"
+BACKUP_FILENAME="$BACKUP_DIR/$DB_DATABASE.sql"
 
 sudo docker exec -i $DB_HOST mysql -h $DB_HOST -u $DB_USER -p$DB_PASS --force  $DB_DATABASE < $BACKUP_FILENAME
